f9246230ab79f89473bfbd9276e258717b588326
Gavin Mogan <gmogan@cloudbees.com>
2019-04-16 12:38:25 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express')
 const rp = require('request-promise-native')
 require('express-csv')
 const app = express()
-
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res, next) {
